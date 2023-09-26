62cbcbcd9ce343ed8363d83f0531b4e2a48a0b8a
atomar <atomar@LAP-55358>
2023-09-26 10:44:51 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class poro
+{
+     static void Main(String[] ar)
+    {
+        try // using try catch just for demonstration purpose
+        {     
+            string demoVar1 = "11";
+            int i = Convert.ToInt32(demoVar1);
+            string a = "Ayush";
+            string b = "Singh Tomar";
+            Console.WriteLine("Hi {0} {1}", a,b);
+            Console.WriteLine(i);
+        }
+        catch(Exception) {
+            Console.WriteLine("use only numbers for demovar1");
+
+        }
+        int demoVar2 = 'A';
+        string @int = "34";
+        Console.WriteLine(@int);    
+
+        Console.WriteLine((char)(demoVar2+1));
+       
+    }
+}
